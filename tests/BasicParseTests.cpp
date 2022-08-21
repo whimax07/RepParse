4ccd78851b486ed80de9b688ac677fba460db9a5
adhowell <a.howell@live.co.uk>
2022-08-21 09:47:29 +0100

    Added test for NEGATE symbol

--- tests/BasicParseTests.cpp
+++ tests/BasicParseTests.cpp
@@ -53,19 +53,20 @@ TEST(RepParseBasic, ExtraSpacesParse) {
 
 
 TEST(RepParseBasic, AllOperatorsParse) {
-    auto singleAddNoSpaces = Tokenizer::tokenize("10+((0xFF/2)*4)");
-    EXPECT_TRUE(singleAddNoSpaces.size() == 11);
-    EXPECT_TRUE(*singleAddNoSpaces[0] == NumericToken("10"));
-    EXPECT_TRUE(*singleAddNoSpaces[1] == symbols::ADD);
-    EXPECT_TRUE(*singleAddNoSpaces[2] == symbols::OPEN_BRACKET);
-    EXPECT_TRUE(*singleAddNoSpaces[3] == symbols::OPEN_BRACKET);
-    EXPECT_TRUE(*singleAddNoSpaces[4] == NumericToken("0xFF"));
-    EXPECT_TRUE(*singleAddNoSpaces[5] == symbols::DIVIDE);
-    EXPECT_TRUE(*singleAddNoSpaces[6] == NumericToken("2"));
-    EXPECT_TRUE(*singleAddNoSpaces[7] == symbols::CLOSE_BRACKET);
-    EXPECT_TRUE(*singleAddNoSpaces[8] == symbols::MULTIPLY);
-    EXPECT_TRUE(*singleAddNoSpaces[9] == NumericToken("4"));
-    EXPECT_TRUE(*singleAddNoSpaces[10] == symbols::CLOSE_BRACKET);
+    auto allOps = Tokenizer::tokenize("-10+( (0xFF/2)*4) ");
+    EXPECT_TRUE(allOps.size() == 12);
+    EXPECT_TRUE(*allOps[0] == symbols::NEGATE);
+    EXPECT_TRUE(*allOps[1] == NumericToken("10"));
+    EXPECT_TRUE(*allOps[2] == symbols::ADD);
+    EXPECT_TRUE(*allOps[3] == symbols::OPEN_BRACKET);
+    EXPECT_TRUE(*allOps[4] == symbols::OPEN_BRACKET);
+    EXPECT_TRUE(*allOps[5] == NumericToken("0xFF"));
+    EXPECT_TRUE(*allOps[6] == symbols::DIVIDE);
+    EXPECT_TRUE(*allOps[7] == NumericToken("2"));
+    EXPECT_TRUE(*allOps[8] == symbols::CLOSE_BRACKET);
+    EXPECT_TRUE(*allOps[9] == symbols::MULTIPLY);
+    EXPECT_TRUE(*allOps[10] == NumericToken("4"));
+    EXPECT_TRUE(*allOps[11] == symbols::CLOSE_BRACKET);
 }
 
 
