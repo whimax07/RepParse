855dddc9899b39ed00130864cd332a8b24ed93d1
max <m.d.whitehouse@outlook.com>
2022-11-30 20:46:18 +0000

    Added the first basic test for ast eval.

--- tests/AstEvalTests.cpp
+++ tests/AstEvalTests.cpp
@@ -2,3 +2,26 @@
 // Created by max on 29/11/22.
 //
 
+#include <gtest/gtest.h>
+
+#include "RepParse.h"
+
+
+using namespace repper;
+
+bool eq(const TypedNumbers& lhs, auto rhs) {
+    return std::visit([rhs](auto in) { return in == rhs; }, lhs);
+}
+
+
+
+TEST(AstEval, basic) {
+    auto ast1 = RepParse::parse("5 * 6");
+    auto res1 = AST::evaluate(make_shared<AST>(ast1));
+    ASSERT_TRUE(eq(res1, 30));
+
+    auto ast2 = RepParse::parse("106168440 & 0xff");
+    auto res2 = AST::evaluate(make_shared<AST>(ast2));
+    ASSERT_TRUE(eq(res2, 120));
+    ASSERT_FALSE(eq(res2, 20));
+}
